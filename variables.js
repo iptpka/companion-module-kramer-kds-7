d71a2d00a5c353b0a7b13575f0791a30049283d7
Tuomas Järvenkylä <tuomasjarvenkyla@gmail.com>
2024-05-08 10:20:55 +0300

    remove test variables

--- variables.js
+++ variables.js
@@ -1,10 +1,6 @@
 export function getVariableDefinitions (self) {
 	const variables = [
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-		{ variableId: 'tcp_response', name: 'Tcp Response'}
+		{ variableId: 'tcp_response', name: 'Tcp Response'},
 	]
-
 	return variables
 }
